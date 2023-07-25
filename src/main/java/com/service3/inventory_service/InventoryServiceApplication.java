1c8e72d6ef436c10f734b5cea25c97d9311f5cba
Sebastiano <RZUSST02B24C351W@studium.unict.it>
2023-07-25 16:50:49 +0200

    Main Args

--- src/main/java/com/service3/inventory_service/InventoryServiceApplication.java
+++ src/main/java/com/service3/inventory_service/InventoryServiceApplication.java
@@ -1,13 +1,35 @@
 package com.service3.inventory_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.service3.inventory_service.repository.InventoryRepository;
+import com.service3.inventory_service.model.Inventory;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args ->{
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+			
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("ps10");
+			inventory1.setQuantity(0);
+			
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+		
 	}
 
 }
